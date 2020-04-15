a86e28f98fee8cac93579782a518998c19057e08
Davide Benvegnu <davide.benvegnu@outlook.com>
2020-04-15 14:20:13 +0800

    Added GH PAT

--- migrate.ps1
+++ migrate.ps1
@@ -5,6 +5,7 @@ $AzDOPrj = Read-Host 'Insert Azure DevOps Project name'
 $AzDORepo = Read-Host 'Insert Azure DevOps Repo name'
 Write-Host ' '
 Write-Host 'GITHUB PARAMS'
+$GHPAT = Read-Host 'Insert GitHub PAT'
 $GHUser = Read-Host 'Insert GitHub Usernname'
 $GHRepo = Read-Host 'Insert GitHub Repo name'
 
@@ -35,12 +36,14 @@ git checkout master
 
 # STEP 2: Add a "new repo" as a new remote origin:
 $GHoriginUrl = "https://github.com/$GHUser/$GHRepo.git"
-git remote add new-origin $GHoriginUrl
+git remote add GHorigin $GHoriginUrl
 
 # STEP 3: Push all local branches and tags to the new repo.
-git push --all new-origin
+$GHpushUrl = "https://$GHPAT@github.com/$GHUser/$GHRepo.git"
 
-git push --tags new-origin
+git push --all $GHpushUrl
+
+git push --tags $GHpushUrl
 
 # STEP 4. Clean up. Remove "old repo" origin and its dependencies, and rename new origin
 
@@ -48,7 +51,7 @@ git remote -v
 
 git remote rm origin
 
-git remote rename new-origin origin
+git remote rename GHorigin origin
 
 ### Done! Now your local git repo is connected to "new repo" remote
 ### which has all the branches, tags and commits history.
